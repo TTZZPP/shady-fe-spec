3ea49245938f90cc6dc0d6ecedce3afdd83001c3
SLIM-SHADYYYY <1141791429@qq.com>
2025-04-14 09:03:27 +0800

    fix: eslint-plugin默认推荐配置

---
+++ packages/eslint-plugin/configs/recommended.js
@@ -0,0 +1,7 @@
+export default {
+  plugins: ['shady-fe-eslint-plugin'],
+  rules: {
+    'shady-fe-eslint-plugin/no-http-url': 'warn',
+    'shady-fe-eslint-plugin/no-secret-info': 'error',
+  },
+};
